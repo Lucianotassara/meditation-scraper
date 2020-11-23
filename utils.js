8bb98decab66673e601afa0797e8852d099b0944
Luciano Tassara <lucianotassara@gmail.com>
2020-11-23 00:11:51 -0300

    deploy

--- utils.js
+++ utils.js
@@ -16,14 +16,14 @@ async function getRvcVerseAPI(key){
 }
 
 async function getLastScrapedDate(){
-    evidaApiUrl += `/lastDateScraped`;
+    // evidaApiUrl += `/lastDateScraped`;
     try {
-        const response = await fetch(evidaApiUrl);
+        const response = await fetch(evidaApiUrl + '/lastDateScraped');
         const json = await response.json();
         console.log(`Haciendo fetch a evida-api para obtener ultima fecha disponible: ${evidaApiUrl}`)
         console.log(`evida-api responde: ${JSON.stringify( json )}`);
         
-        const futureDate = new Date(json.fecha);
+        const futureDate = new Date(json.fecha + 1 );
         futureDate.setHours(0,0,0,0);
         return futureDate;
 
